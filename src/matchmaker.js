896462a3acb8e4338de101eea1655656687da5d8
Felix Breitenbach <felix.breitenbach@gmail.com>
2019-11-16 18:03:14 +0100

    start matchmaker implementation

---
+++ src/matchmaker.js
@@ -0,0 +1,11 @@
+module.exports = function Matchmaker(players) {
+  // init
+  console.log(players.length);
+
+  // return matchmacker
+  return {
+    draw: function() {
+      // draw next round
+    }
+  };
+};
---
+++ src/testMatchmaker.js
@@ -0,0 +1,10 @@
+const Matchmaker = require("./matchmaker.js");
+const fs = require("fs");
+
+const data = fs.readFileSync("assets/tournament.json");
+const json = JSON.parse(data);
+
+// init matchmaker with player data
+let players = json.tournament.competition.players.player;
+
+const matchmaker = Matchmaker(players);
