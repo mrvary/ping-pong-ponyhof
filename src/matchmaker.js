ae3baae46ab9ec6eacee90309656540ed935ddc3
Felix Breitenbach <felix.breitenbach@gmail.com>
2019-11-16 18:33:26 +0100

    clean input for matchmaker

--- src/matchmaker.js
+++ src/matchmaker.js
@@ -1,7 +1,7 @@
-module.exports = function Matchmaker(players) {
-  // init
-  console.log(players.length);
+module.exports = function Matchmaker(participants) {
+  let players = initPlayers(participants);
 
+  console.log(players);
   // return matchmacker
   return {
     draw: function() {
@@ -9,3 +9,23 @@ module.exports = function Matchmaker(players) {
     }
   };
 };
+
+function initPlayers(participants) {
+  return participants.map(participant => {
+    // destructure input
+    const { id, person } = participant;
+    const { firstname, lastname, ttr } = person;
+    const clubname = person["club-name"];
+
+    return {
+      id,
+      firstname,
+      lastname,
+      clubname,
+      gamesWon: 0,
+      // Buchholz-number, number of games that enemies won
+      vs: {},
+      ttr
+    };
+  });
+}
