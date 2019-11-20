eb58d867ed65c915f22bac6ea8a0458fec825a66
Daniel <daniel.n96@t-online.de>
2019-11-20 10:19:15 +0100

    developmer mode implemented

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createWindow() {
   win = new BrowserWindow({
     width: 800,
     height: 600,
-    fullscreen:developerMode,
+    fullscreen: developerMode,
     webPreferences: {
       nodeIntegration: true
     }
