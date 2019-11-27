2609f5acebaf96a01618790ecd6f0edde3bf9181
LAP-BOOK-NOTE-T\willi <superhamster95@gmx.de>
2019-11-27 16:27:01 +0100

    dashboard erste schritte

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./App.css";
+//import logo from ;
 //import log from "electron-log";
-
+//<img src="bild30.png" alt="Logo" width="100" height="100"/>
+//<a className="delete"></a>
 const log = window.require('electron-log');
 const ipc = window.require('electron').ipcRenderer;
-
+/*<button
+          className="button-game"
+          id="xml-import"
+          onClick={() => ipc.send("open-import-dialog")}
+        >
+          import XML
+        </button>
+        <button
+          className="button-game"
+          id="close"
+          onClick={() => ipc.send("close-application")}
+        >
+          close
+        </button>
+      */
 function App() {
   ipc.on("opened-import-dialog", (event, players) => {
     log.info(players);
@@ -13,29 +29,22 @@ function App() {
   return (
     <div>
       <header>
-        <h1>yo</h1>
-        <button
-          class="button"
-          id="openClient"
-          onClick={() => ipc.send("open-client")}
-        >
-          Open Client
-        </button>
-        <button
-          class="button"
-          id="xml-import"
-          onClick={() => ipc.send("open-import-dialog")}
-        >
-          import XML
-        </button>
-        <button
-          class="button"
-          id="close"
-          onClick={() => ipc.send("close-application")}
-        >
-          close
-        </button>
-      </header>
+        <h1>PingPongPonyhof</h1>
+      </header>        
+        <ul>
+          <li className="ausrichtungcheck">
+            
+            <button className="button-game">Spiele</button>
+            <button className="button-delete">Löschen</button>
+            
+          </li>
+          <li className="ausrichtungcheck">
+            <button className="button-game">Spiele</button>
+            <button className="button-delete">Löschen</button>
+            
+          </li>
+        </ul>
+      
     </div>
   );
 }
