05701b71f6169e21dbe5473109b3e764656f9ca3
LAP-BOOK-NOTE-T\willi <superhamster95@gmx.de>
2019-12-04 14:54:59 +0100

    aktuelles von anderen spielen getrennt

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "./App.css";
-import ReactDOM from "react-dom";
 import 'bulma/css/bulma.css';
-
 
 //import logo from ;
 //import log from "electron-log";
 //<img src="bild30.png" alt="Logo" width="100" height="100"/>
 //<a className="delete"></a>
-const log = window.require('electron-log');
-const ipc = window.require('electron').ipcRenderer;
+
 /*<button
           className="button-game"
           id="xml-import"
@@ -18,53 +15,80 @@ const ipc = window.require('electron').ipcRenderer;
           import XML
         </button>
 */
-var gamesListe = ["23.7.2019","11.8.2019","7.9.2019","22.9.2019", "2.10.2019", "21.11.2019"]
 
-function refresh(){
-  ReactDOM.render(<App/>, 
-  document.getElementById('root'))
-}
 
-function liste(){
-  var htmlList =[];
-  for (var i = 0; i<gamesListe.length; i++){
-    htmlList.push(listElement(i));
+class App extends React.Component{
+
+  //TODO: redo connection
+  //const log = window.require('electron-log');
+  //const ipc = window.require('electron').ipcRenderer;
+  //ipc.on("opened-import-dialog", (event, players) => {
+  //  log.info(players);
+  //});
+
+  constructor(props){
+      super(props);
+      this.state = {
+        gamesListe : [],
+        laufendesSpiel : true
+    };
   }
-  return htmlList;
-}
+  gamesListe = ["23.7.2019","11.8.2019","7.9.2019","22.9.2019", "2.10.2019", "21.11.2019"];
+  laufendesSpiel = true;
 
-function listElement(i){
-  return(
-    <li className="ausrichtungcheck">
-      <button className="button-game">Spiel vom {gamesListe[i]}</button>
-      <div className="button-game"/>
-      <button className="button-delete" onClick={(e) => removeFromList(gamesListe[i], e)}>Löschen</button>
-    </li>
-  )
-}
-
-function removeFromList(elem){
-  gamesListe.splice( gamesListe.indexOf(elem),1);
-  refresh();
-}
-
-
-function App() {
-  
-  ipc.on("opened-import-dialog", (event, players) => {
-    log.info(players);
-  });
-  return (
-    
+  render (){
+    return (
     <div>
       <header>
         <h1>PingPongPonyhof</h1>
       </header> 
+      {this.aktuellesSpiel()}
+      <h3>Alten Spiele</h3>
       <ul>
-        {liste()}
+        {this.liste()}
       </ul>
     </div>
-  );
+    );
+  }
+
+  liste(){
+    var htmlList =[];
+    for (var i = 1; i<this.gamesListe.length; i++){
+      htmlList.push(this.listElement(i));
+    }
+    return htmlList;
+  }
+  
+  listElement(i){
+    return(
+      <li className="ausrichtungcheck">
+        <button className="button-game">Spiel vom {this.gamesListe[i]}</button>
+        <div className="button-game"/>
+        <button className="delete button-delete" onClick={(e) => this.removeFromList(this.gamesListe[i], i, e)}>Löschen</button>
+      </li>
+    )
+  }
+  
+  removeFromList(elem, position){
+    if (position === 0){
+      this.laufendesSpiel = false
+    }
+    this.setState(this.gamesListe.splice( this.gamesListe.indexOf(elem),1));
+  }
+  aktuellesSpiel(){
+    if (this.laufendesSpiel===true){
+      return(
+        <div> 
+          <h3>Aktuelles Spiel</h3>
+          <li className="ausrichtungcheck">
+            <button className="button-game">Spiel vom {this.gamesListe[0]}</button>
+            <div className="button-game"/>
+             <button className="delete button-delete" onClick={(e) => this.removeFromList(this.gamesListe[0], 0, e )}>Löschen</button>
+          </li>
+        </div>
+      )
+    }
+  }
 }
 
 
