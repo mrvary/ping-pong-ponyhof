40a87ed5b457c0e96faf350ed6fd4d5c4b5284e1
Dietze, Sophia <sophia.dietze@student.fhws.de>
2019-12-11 14:30:24 +0100

    App Version2 - functions

--- src/app/App.js
+++ src/app/App.js
@@ -1,129 +1,90 @@
-import React from "react";
+//import React from "react";
 import "./App.css";
-import 'bulma/css/bulma.css';
+import "bulma/css/bulma.css";
+import ReactDOM from "react-dom";
+import React, { useState } from "react";
+import { statement } from "@babel/template";
 
-//import logo from ;
-//import log from "electron-log";
-//<img src="bild30.png" alt="Logo" width="100" height="100"/>
-//<a className="delete"></a>
+const refresh = () => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
 
-/*<button
-          className="button-game"
-          id="xml-import"
-          onClick={() => ipc.send("open-import-dialog")}
-        >
-          import XML
-        </button>
-*/
-
-
-class App extends React.Component{
-
-  //TODO: redo connection
-  //const log = window.require('electron-log');
-  //const ipc = window.require('electron').ipcRenderer;
-  //ipc.on("opened-import-dialog", (event, players) => {
-  //  log.info(players);
-  //});
-
-  constructor(props){
-      super(props);
-      this.state = {
-        gamesListe : [],
-        laufendesSpiel : true
-    };
-  }
-  gamesListe = ["23.7.2019","11.8.2019","7.9.2019","22.9.2019", "2.10.2019", "21.11.2019"];
-  laufendesSpiel = true;
-
-  render (){
-    return (
-    
-     
-<body >
-
-
-<section class="hero is-medium is-primary is-bold">
-       <div class="hero-body">
-       <div class="container has-text-centered">
-
-         <h1 class="title">
-           PingPongPonyhof
-         </h1>
+const Header = props => {
+  return (
+    <section class="hero is-medium is-primary is-bold">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">{props.title}</h1>
         </div>
-       </div>
-      </section>
+      </div>
+    </section>
+  );
+};
 
-  
-     <section class="section">
-      {this.aktuellesSpiel()}
-      </section>
+const ButtonZeile = probs => {
+  const datum = probs.datum;
+  const löschen = probs.löschen;
+  const id = probs.id;
 
-      <section class="section">
-      <h3>Alten Spiele</h3>
-      <ul>
-        {this.liste()}
-      </ul>
-      </section>
-      
+  return (
+    <li className="ausrichtungcheck">
+      <button className="button-game">Spiel vom {datum}</button>
+      <div className="button-game" />
+      <button className="button-delete" onClick={() => löschen(id)}>
+        Löschen
+      </button>
+    </li>
+  );
+};
 
-      <section class="section">
-      <footer class="footer">
-        <div class="content has-text-centered">
-          <p>
-            <strong>PingPongPonyhof</strong> by coolest guys ever. 
-          </p>
-        </div>
-      </footer>
-      </section>
+const ButtonListe = probs => {
+  //const [liste] = useState(0);
+  const liste = probs.liste;
 
-      </body>
+  const Delete = id => {
+    if (id === 0) {
+    }
+    liste.splice(liste.indexOf(id), 1);
+  };
 
-
-
+  var htmlList = [];
+  for (var i = 1; i < liste.length; i++) {
+    htmlList.push(
+      <ButtonZeile datum={liste[i]} id={i} löschen={Delete.bind(this)} />
     );
+  }
+  return htmlList;
+};
 
-  }
+const Footer = probs => {
+  return (
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>{probs.title}</strong> by coolest guys ever.
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-  liste(){
-    var htmlList =[];
-    for (var i = 1; i<this.gamesListe.length; i++){
-      htmlList.push(this.listElement(i));
-    }
-    return htmlList;
-  }
-  
-  listElement(i){
-    return(
-      <li className="ausrichtungcheck">
-        <button className="button-game">Spiel vom {this.gamesListe[i]}</button>
-        <div className="button-game"/>
-        <button className="button-delete" onClick={(e) => this.removeFromList(this.gamesListe[i], i, e)}>Löschen</button>
-      </li>
-    )
-  }
-  
-  removeFromList(elem, position){
-    if (position === 0){
-      this.laufendesSpiel = false
-    }
-    this.setState(this.gamesListe.splice( this.gamesListe.indexOf(elem),1));
-  }
-  aktuellesSpiel(){
-    if (this.laufendesSpiel===true){
-      return(
-        <div> 
-          <h3>Aktuelles Spiel</h3>
-          <li className="ausrichtungcheck">
-            <button className="button-game">Spiel vom {this.gamesListe[0]}</button>
-            <div className="button-game"/>
-             <button className="button-delete" onClick={(e) => this.removeFromList(this.gamesListe[0], 0, e )}>Löschen</button>
-          </li>
-        </div>
-      )
-    }
-  }
-}
+const App = () => {
+  var gamesListe = [
+    "23.7.2019",
+    "11.8.2019",
+    "7.9.2019",
+    "22.9.2019",
+    "2.10.2019",
+    "21.11.2019"
+  ];
 
+  return (
+    <div>
+      <Header title="PingPongPonyhof" />
+      <ButtonListe liste={gamesListe} />
+      <Footer title="PingPongPonyhof" />
+    </div>
+  );
+};
 
 export default App;
