de13843050f32917d3bbb0f851a4a833cab34734
mrvary <felix.breitenbach@gmail.com>
2019-12-12 01:53:00 +0100

    change login form

--- client/src/react/components/Login.js
+++ client/src/react/components/Login.js
@@ -12,10 +12,32 @@ function Login({
   }
 
   return (
-    <div>
-      Tischnummer eingeben:
-      <input type="number" value={tableNumber} onChange={tableNumberChanged} />
-      <button onClick={sendTableNumber}>OK</button>
+    <div className="login">
+      <form className="login-form">
+        <datalist id="table-numbers">
+          <option value="0" />
+          <option value="1" />
+          <option value="2" />
+          <option value="3" />
+          <option value="4" />
+        </datalist>
+        <label for="login-input-field" className="login-text">
+          Tischnummer
+        </label>
+        <break />
+        <input
+          className="login-input"
+          name="login-input-field"
+          type="number"
+          min="0"
+          list="table-numbers"
+          value={tableNumber}
+          onChange={tableNumberChanged}
+        />
+        <button className="login-button" onClick={sendTableNumber}>
+          OK
+        </button>
+      </form>
     </div>
   );
 }
