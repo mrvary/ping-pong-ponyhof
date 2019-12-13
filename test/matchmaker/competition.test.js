2602ba9ab8e91f32c1e9b89ae0f3c0f762f4be4b
mrvary <felix.breitenbach@gmail.com>
2019-12-13 01:11:06 +0100

    test _sortPlayersBy()

--- test/matchmaker/competition.test.js
+++ test/matchmaker/competition.test.js
@@ -1,8 +1,14 @@
 const {
   _createPlayer,
+  _sortPlayersBy,
   createPlayersFromJSON
 } = require('../../src/matchmaker/competition');
-const { inputPlayers, tournamentJSON } = require('./competition.test.data');
+
+const {
+  inputPlayers,
+  cleanedUpPlayers,
+  tournamentJSON
+} = require('./competition.test.data');
 
 const EXPECTED_PLAYER = {
   id: 'PLAYER1',
@@ -32,5 +38,18 @@ describe('createPlayers()', () => {
 
   test('with correct player objects', () => {
     expect(players[0]).toEqual(EXPECTED_PLAYER);
+  });
+});
+
+describe('sortPlayersBy()', () => {
+  test('sorts players by qttr (ascending)', () => {
+    const [p1, p2, p3] = _sortPlayersBy(cleanedUpPlayers, 'qttr');
+    expect(p1.qttr).toBeGreaterThanOrEqual(p2.qttr);
+    expect(p2.qttr).toBeGreaterThanOrEqual(p3.qttr);
+  });
+  test.only('sorts players by games won (ascending)', () => {
+    const [p1, p2, p3] = _sortPlayersBy(cleanedUpPlayers, 'gamesWon');
+    expect(p1.gamesWon).toBeGreaterThanOrEqual(p2.gamesWon);
+    expect(p2.gamesWon).toBeGreaterThanOrEqual(p3.gamesWon);
   });
 });
