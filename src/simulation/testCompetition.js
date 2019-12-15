96ee5707f6f5764f403af82d3b90810692bebb8b
Felix Breitenbach <felix.breitenbach@gmail.com>
2019-12-15 04:02:40 +0100

    move files to folder and add yarn command start-simulation

--- src/simulation/testCompetition.js
+++ src/simulation/testCompetition.js
@@ -1,7 +1,7 @@
 const Competition = require("./competition.js");
 const fs = require("fs");
 
-const data = fs.readFileSync("assets/tournament.json");
+const data = fs.readFileSync("../../assets/tournament.json");
 const json = JSON.parse(data);
 
 // init matchmaker with player data
