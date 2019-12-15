337c68ddd2426e8d5b1c7f0da37ed60a4e9c7b8a
Felix Breitenbach <felix.breitenbach@gmail.com>
2019-12-15 03:18:16 +0100

    create player.js and move related code

--- src/matchmaker/competition.js
+++ src/matchmaker/competition.js
@@ -1,28 +1,9 @@
-// INIT
-function createPlayersFromJSON(json) {
-  // players are deeply nested in the input json
-  const players = json.tournament.competition.players.player;
-
-  return players.map(_createPlayer);
-}
-
-function _createPlayer(dataFromJSON) {
-  const { id, person } = dataFromJSON;
-  const { firstname, lastname, ttr } = person;
-  const clubname = person["club-name"];
-
-  return {
-    id,
-    firstname,
-    lastname,
-    clubname,
-    gamesWon: 0,
-    matchIds: [],
-    qttr: parseInt(ttr, 10),
-    active: true,
-    hasFreeTicket: false
-  };
-}
+const {
+  _sortPlayersBy,
+  _separateTopFromBottomPlayers,
+  _updatePlayers,
+  _pairPlayers
+} = require("./player.js");
 
 // DRAW ROUNDS
 function drawRound({ players }) {
@@ -48,17 +29,20 @@ function _drawFirstRound({ players }) {
   // 4. create matches
   const newMatches = _createMatches(pairings);
 
-  // -> update players?
+  // 5. update players
+  const newPlayers = _updatePlayers(newMatches);
 
-  return { players, matches: newMatches };
+  return { players: newPlayers, matches: newMatches };
 }
 
-function _drawLaterRound({ players }) {}
+function _drawLaterRound({ players }) {
+  // todo
+}
 
 function _createMatches({ pairings }) {
-  let matches = [];
   let remainingPairings = pairings;
 
+  let matches = [];
   while (remainingPairings) {
     const match = _createMatch(remainingPairings.shift());
     matches.push(match);
@@ -67,6 +51,7 @@ function _createMatches({ pairings }) {
   return matches;
 }
 
+// todo: pass round and matchId
 function _createMatch({ player1, player2 }, round, matchId) {
   // early return when no second player
   if (!player2) {
@@ -94,70 +79,11 @@ function _createMatch({ player1, player2 }, round, matchId) {
   return match;
 }
 
-function _pairPlayers({ top, bottom }) {
-  let bottomPlayers = bottom;
-  let topPlayers = top;
-  let pairings = [];
-
-  while (bottomPlayers.length !== 0) {
-    const randomTopPlayer =
-      topPlayers[Math.floor(Math.random() * topPlayers.length)];
-    const randomBottomPlayer =
-      bottomPlayers[Math.floor(Math.random() * bottomPlayers.length)];
-
-    //remove chosen players
-    topPlayers = topPlayers.filter(player => player !== randomTopPlayer);
-    bottomPlayers = bottomPlayers.filter(
-      player => player !== randomBottomPlayer
-    );
-
-    pairings.push({ player1: randomTopPlayer, player2: randomBottomPlayer });
-  }
-
-  // pair last player when odd number of players
-  if (topPlayers[0]) {
-    pairings.push({ player1: topPlayers[0] });
-  }
-
-  return pairings;
-}
-
-function _separateTopFromBottomPlayers(players) {
-  const sortedPlayers = _sortPlayersBy(players, "qttr");
-  const top = sortedPlayers.slice(0, Math.ceil(sortedPlayers.length / 2));
-  const bottom = sortedPlayers.slice(Math.ceil(sortedPlayers.length / 2));
-
-  return { top, bottom };
-}
-
-// UTILITY FUNCTIONS
-function _sortPlayersBy(players, selector) {
-  return players.sort((playerA, playerB) => {
-    return playerB[selector] - playerA[selector];
-  });
-}
-
-function _shuffle(array) {
-  const TIMES = array.length;
-  for (let i = 0; i < TIMES; i++) {
-    const first = Math.floor(Math.random() * array.length);
-    const second = Math.floor(Math.random() * array.length);
-    [array[first], array[second]] = [array[second], array[first]];
-  }
-  return array;
-}
-
 module.exports = {
   // pubic
-  createPlayersFromJSON,
   drawRound,
 
   // private
-  _createPlayer,
   _drawFirstRound,
-  _drawLaterRound,
-  _pairPlayers,
-  _sortPlayersBy,
-  _separateTopFromBottomPlayers,
-  _shuffle
+  _drawLaterRound
 };
