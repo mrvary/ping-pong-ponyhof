1badad20371c8bc62418e0ffaa9a58eea4a1f10f
Felix Breitenbach <felix.breitenbach@gmail.com>
2019-12-15 03:28:48 +0100

    move match code into match.js and move tests also into separate files

--- src/matchmaker/competition.js
+++ src/matchmaker/competition.js
@@ -1,82 +1,44 @@
 const {
-  _sortPlayersBy,
-  _separateTopFromBottomPlayers,
-  _updatePlayers,
-  _pairPlayers
+  sortPlayersBy,
+  separateTopFromBottomPlayers,
+  updatePlayers,
+  pairPlayers
 } = require("./player.js");
+
+const { createMatches } = require("./match.js");
 
 // DRAW ROUNDS
 function drawRound({ players }) {
   const isLaterRound = players[0].matchIds.length > 0;
 
   if (!isLaterRound) {
-    return _drawLaterRound(players);
+    return drawLaterRound(players);
   }
 
-  return _drawFirstRound(players);
+  return drawFirstRound(players);
 }
 
-function _drawFirstRound({ players }) {
+function drawFirstRound({ players }) {
   // 1. sort players by qttr
-  const sortedPlayers = _sortPlayersBy(players, "qttr");
+  const sortedPlayers = sortPlayersBy(players, "qttr");
 
   // 2. separate top and bottom players
-  const { top, bottom } = _separateTopFromBottomPlayers(sortedPlayers);
+  const { top, bottom } = separateTopFromBottomPlayers(sortedPlayers);
 
   // 3. pair players together
-  const pairings = _pairPlayers({ top, bottom });
+  const pairings = pairPlayers({ top, bottom });
 
   // 4. create matches
-  const newMatches = _createMatches(pairings);
+  const newMatches = createMatches(pairings);
 
   // 5. update players
-  const newPlayers = _updatePlayers(newMatches);
+  const newPlayers = updatePlayers(newMatches);
 
   return { players: newPlayers, matches: newMatches };
 }
 
-function _drawLaterRound({ players }) {
+function drawLaterRound({ players }) {
   // todo
-}
-
-function _createMatches({ pairings }) {
-  let remainingPairings = pairings;
-
-  let matches = [];
-  while (remainingPairings) {
-    const match = _createMatch(remainingPairings.shift());
-    matches.push(match);
-  }
-
-  return matches;
-}
-
-// todo: pass round and matchId
-function _createMatch({ player1, player2 }, round, matchId) {
-  // early return when no second player
-  if (!player2) {
-    const freeTicketMatch = {
-      id: matchId,
-      player1: { ...player1, matchIds: player1.matchIds.concat(matchId) },
-      round,
-      result: [],
-      sets: [],
-      freeTicket: true
-    };
-    return freeTicketMatch;
-  }
-
-  const match = {
-    id: matchId,
-    player1: { ...player1, matchIds: player1.matchIds.concat(matchId) },
-    player2: { ...player2, matchIds: player2.matchIds.concat(matchId) },
-    round,
-    result: [],
-    sets: [],
-    freeTicket: false
-  };
-
-  return match;
 }
 
 module.exports = {
@@ -84,6 +46,6 @@ module.exports = {
   drawRound,
 
   // private
-  _drawFirstRound,
-  _drawLaterRound
+  drawFirstRound,
+  drawLaterRound
 };
---
+++ test/matchmaker/match.test.js
@@ -0,0 +1,5 @@
+const { createMatch, createMatches } = require("../../src/matchmaker/match");
+
+describe("createMatch", () => {
+  test.todo("creates an array of matches from an array of pairings");
+});
