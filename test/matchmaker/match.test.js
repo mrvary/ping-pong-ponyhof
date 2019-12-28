250806bd92a1a469613206251ecaaefc0e1fae89
Felix Breitenbach <felix.breitenbach@gmail.com>
2019-12-28 17:20:38 +0100

    more tests for create matches

--- test/matchmaker/match.test.js
+++ test/matchmaker/match.test.js
@@ -6,20 +6,45 @@ const {
 } = require("../../src/matchmaker/player");
 
 describe("createMatches()", () => {
-  const evenNumberOfPlayers = cleanedUpPlayers
-    .map(player => ({ ...player, qttr: player.qttr + 100 }))
+  const players = cleanedUpPlayers
+    .map(player => ({
+      ...player,
+      firstname: player.firstname + " Copy",
+      id: player.id + "0",
+      qttr: player.qttr + 100
+    }))
     .concat(cleanedUpPlayers);
 
-  const evenTopAndBottomPlayers = separateTopFromBottomPlayers(
-    evenNumberOfPlayers
-  );
-  const evenlyPairedPlayers = pairPlayers(evenTopAndBottomPlayers);
+  const separatedPlayers = separateTopFromBottomPlayers(players);
+  const pairings = pairPlayers(separatedPlayers);
+  const matches = createMatches(pairings);
 
   test("creates an array of matches from an array of pairings", () => {
-    console.log(evenlyPairedPlayers);
-    console.log(createMatches(evenlyPairedPlayers));
+    let matchIds = new Set();
+    let players = new Set();
+
+    matches.forEach(match => {
+      matchIds.add(match.id);
+      players.add(match.player1.id);
+      players.add(match.player2.id);
+    });
+
+    const matchCount = matches.length;
+
+    expect(matchCount).toBe(pairings.length);
+    expect(matchIds.size).toBe(matchCount);
+    expect(players.size).toBe(matchCount * 2);
+
+    matchIds = Array.from(matchIds);
+    for (let index = 0; index < matchIds.length - 1; index++) {
+      expect(matchIds[index]).toBe(matchIds[index + 1] - 1);
+    }
+
+    matchIds.reduce((prev, current) => {
+      expect(current).toBe(prev + 1);
+      return current;
+    });
   });
-  test.todo("increments the matchId between two executions");
 });
 
 describe("createMatch", () => {
