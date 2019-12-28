60d193e257d0fa9eba2c1355494c27a1e7874d73
Felix Breitenbach <felix.breitenbach@gmail.com>
2019-12-28 15:07:45 +0100

    test if match properties are defined

--- test/matchmaker/match.test.js
+++ test/matchmaker/match.test.js
@@ -1,11 +1,22 @@
 const { createMatch, createMatches } = require("../../src/matchmaker/match");
+const { cleanedUpPlayers } = require("./player.test.data");
 
 describe("createMatches()", () => {
   test.todo("creates an array of matches from an array of pairings");
 });
 
 describe("createMatch", () => {
-  test.todo("creates match that has all the necessary properties");
-  test.todo("increments the matchId between two executions");
+  const [player1, player2] = cleanedUpPlayers;
+  const match = createMatch({ player1, player2 });
+
+  test.only("creates match that has all the necessary properties", () => {
+    expect(match.id).toBeDefined();
+    expect(match.player1).toBeDefined();
+    expect(match.player2).toBeDefined();
+    expect(match.result).toBeDefined();
+    expect(match.sets).toBeDefined();
+    expect(match.freeTicket).toBeDefined();
+  });
+  test("increments the matchId between two executions", () => {});
   test.todo("adds a matchId to player objects");
 });
