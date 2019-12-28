99932df36ab3201844f60ffd5b0b1e527fd00825
Marco Goebel <marco.goebel.1@student.fhws.de>
2019-12-28 19:35:14 +0100

    add mock data

--- client/src/react/components/Match.js
+++ client/src/react/components/Match.js
@@ -4,6 +4,35 @@ import Title from './Title';
 import ConnectionStatus from './ConnectionStatus';
 import ScoreBoard from './ScoreBoard';
 
+const match = {
+  id: 0,
+  player1: {
+    id: 'PLAYER20',
+    firstname: 'Achim Copy',
+    lastname: 'Amthor',
+    clubname: 'SC Baldham-Vaterstetten',
+    gamesWon: 5,
+    matchIds: [0],
+    qttr: 1351,
+    active: true,
+    hasFreeTicket: false
+  },
+  player2: {
+    id: 'PLAYER3',
+    firstname: 'Ulrich',
+    lastname: 'Dietzel',
+    clubname: 'TTC Friedberg',
+    gamesWon: 1,
+    matchIds: [0],
+    qttr: 1111,
+    active: true,
+    hasFreeTicket: false
+  },
+  result: [],
+  sets: [],
+  freeTicket: false
+};
+
 function Match({ appTitle, isConnected }) {
   return (
     <div>
