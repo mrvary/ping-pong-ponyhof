1cffe9d4627711bbed8115b8d21a0f602bca4b05
Marco Goebel <marco.goebel.1@student.fhws.de>
2019-12-31 13:58:32 +0100

    rename i to index

--- client/src/react/components/ScoreBoard.js
+++ client/src/react/components/ScoreBoard.js
@@ -5,14 +5,12 @@ import MainScore from './MainScore';
 import SetScore from './SetScore';
 
 function ScoreBoard({ match }) {
+  const showSets = () => match.sets.map((set, index) => <SetScore key={index} index={index} set={set} />);
+
   return (
     <>
       <MainScore match={match} />
-      <div className="score-board__container">
-        {match.sets.map((set, i) => (
-          <SetScore key={i} index={i} set={set} />
-        ))}
-      </div>
+      <div className="score-board__container">{showSets()}</div>
     </>
   );
 }
