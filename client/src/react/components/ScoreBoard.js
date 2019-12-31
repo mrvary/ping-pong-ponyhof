ec0e143c8c0a0df75109fbbdb780b792058e84df
Marco Goebel <marco.goebel.1@student.fhws.de>
2019-12-31 13:56:15 +0100

    map component directly in html

--- client/src/react/components/ScoreBoard.js
+++ client/src/react/components/ScoreBoard.js
@@ -5,12 +5,14 @@ import MainScore from './MainScore';
 import SetScore from './SetScore';
 
 function ScoreBoard({ match }) {
-  const showSets = () => match.sets.map((set, i) => <SetScore key={i} index={i} set={set} />);
-
   return (
     <>
       <MainScore match={match} />
-      <div className="score-board__container">{showSets()}</div>
+      <div className="score-board__container">
+        {match.sets.map((set, i) => (
+          <SetScore key={i} index={i} set={set} />
+        ))}
+      </div>
     </>
   );
 }
