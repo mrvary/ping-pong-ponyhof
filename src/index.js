3d30aabc0268fd3c0ae02d79dc76f7365e5df9e6
Felix Breitenbach <felix.breitenbach@gmail.com>
2020-01-08 14:58:29 +0100

    resolve conflict

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./react/App";
 
+import "normalize.css";
+import "./index.css";
+
 ReactDOM.render(<App />, document.getElementById("root"));
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './react/App';
-
-import 'normalize.css';
-import './index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> master
