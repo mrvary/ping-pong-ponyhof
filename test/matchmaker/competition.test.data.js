43d937fba9671a9242949304d1e4095b15e73be5
Marco Goebel <marco.goebel.1@student.fhws.de>
2020-01-08 15:00:39 +0100

    add services + simple mock data

---
+++ test/matchmaker/competition.test.data.js
@@ -0,0 +1,22 @@
+const competitions = [
+  { 
+    id: 0, 
+    name: 'BTTV Bavarian TT-Race', 
+    startDate: '25.05.2019',
+    endDate: '25.05.2019',
+    type: 'Einzel',
+    playmode: 'Schweizer System' 
+  },
+  { 
+    id: 1, 
+    name: 'BTTV Bavarian TT-Race', 
+    startDate: '11.08.2019',
+    endDate: '11.08.2019',
+    type: 'Doppel',
+    playmode: 'Schweizer System' 
+  }
+];
+
+module.exports = {
+  competitions
+}
