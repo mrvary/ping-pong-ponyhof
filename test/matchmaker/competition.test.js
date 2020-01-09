910007fe9fed57cff2959aff13f31054ac7db5c4
Daniel <daniel.n96@t-online.de>
2020-01-09 22:51:45 +0100

    rename competition to drawing

--- test/matchmaker/competition.test.js
+++ test/matchmaker/competition.test.js
@@ -1,4 +1,4 @@
-const competition = require("../../src/matchmaker/competition");
+const competition = require("../../src/matchmaker/drawing");
 
 describe("create tests for competition.js", () => {
   test.todo("...");
