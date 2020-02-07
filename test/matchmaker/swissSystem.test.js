efc1fda120161e88a3ac2c3431702373a427bbbd
Daniel <daniel.n96@t-online.de>
2020-02-07 09:29:06 +0100

    add basic swissSystem test

---
+++ test/matchmaker/swissSystem.test.js
@@ -0,0 +1,33 @@
+const { drawFirstRound, drawRound } = require("../../src/matchmaker/drawing");
+
+const {
+  tournamentJSON,
+  tournamentJSON15Players
+} = require("./player.test.data");
+
+const {
+  createPlayersFromJSON,
+  updatePlayers
+} = require("../../src/matchmaker/player");
+
+let players = createPlayersFromJSON(tournamentJSON15Players);
+
+describe("playCompetition", () => {
+  const roundsToPlay = 2;
+
+  for (let round = 1; round <= roundsToPlay; round++) {
+    let matches = drawRound(players);
+    players = updatePlayers(players, matches);
+
+    test("match length", () => {
+      expect(matches.length).toEqual(players.length / 2);
+    });
+    test("gamesWon", () => {
+      let sumGamesWon = 0;
+      players.forEach(player => {
+        sumGamesWon += player.gamesWon;
+      });
+      expect(sumGamesWon).toEqual((round * players.length) / 2);
+    });
+  }
+});
