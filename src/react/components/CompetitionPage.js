9e3d68a904291e5b2df1a0bf7c30a5bce8a7c882
LAP-BOOK-NOTE-T\willi <superhamster95@gmx.de>
2020-02-17 18:10:31 +0100

    added sets to table

--- src/react/components/CompetitionPage.js
+++ src/react/components/CompetitionPage.js
@@ -89,6 +89,11 @@ const TableHeadline = () => {
 };
 
 const TableRow = ({ match }) => {
+  const set1 = match.sets[0][0] + ' : ' + match.sets[0][1];
+  const set2 = match.sets[1][0] + ' : ' + match.sets[1][1];
+  const set3 = 0; //= match.sets[2][0] + ' : ' + match.sets[2][1];
+  const set4 = 0; //= match.sets[3][0] + ' : ' + match.sets[3][1];
+  const set5 = 0; //= match.sets[4][0] + ' : ' + match.sets[4][1];
   return (
     <div className="competitionPage__center-table">
       <div className="competitionPage__table__first-row-alignment">
@@ -102,11 +107,11 @@ const TableRow = ({ match }) => {
           {' '}
           {match.player2}{' '}
         </div>
-        <div className="competitionPage__table__column-alignment"> Satz 1 </div>
-        <div className="competitionPage__table__column-alignment"> Satz 2 </div>
-        <div className="competitionPage__table__column-alignment"> Satz 3 </div>
-        <div className="competitionPage__table__column-alignment"> Satz 4 </div>
-        <div className="competitionPage__table__column-alignment"> Satz 5 </div>
+        <div className="competitionPage__table__column-alignment"> {set1} </div>
+        <div className="competitionPage__table__column-alignment"> {set2} </div>
+        <div className="competitionPage__table__column-alignment"> {set3} </div>
+        <div className="competitionPage__table__column-alignment"> {set4} </div>
+        <div className="competitionPage__table__column-alignment"> {set5} </div>
         <div className="competitionPage__table__column-alignment">
           {' '}
           Ergebnis{' '}
