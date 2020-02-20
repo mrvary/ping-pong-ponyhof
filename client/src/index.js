9c408be4f2d7cbed28f5db338064dda708cc86a3
LAP-BOOK-NOTE-T\willi <superhamster95@gmx.de>
2020-02-20 16:05:21 +0100

    merge

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './react/App';
-
-import 'normalize.css';
-import './index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
--- client/src/react/components/Player/Player.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Player.css';
-
-function Player({ player }) {
-  return <h2>{player.firstname + ' ' + player.lastname}</h2>;
-}
-
-export default Player;
