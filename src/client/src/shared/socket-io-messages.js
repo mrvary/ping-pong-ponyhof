f4bc6c4b93465c83dd727d38d21d51711ccd271a
Felix Breitenbach <felix.breitenbach@gmail.com>
2020-02-24 11:16:21 +0100

    restructure messages

--- src/client/src/shared/socket-io-messages.js
+++ src/client/src/shared/socket-io-messages.js
@@ -1,19 +1,17 @@
 // message for the client-server communication used by socket io
 module.exports = {
-  // generic
   SEND_MESSAGE: "send-message",
-
-  // client -> server messages
   CONNECTION: "connection",
   DISCONNECT: "disconnect",
+
   LOGIN_TABLE: "login-table",
+  LOGIN_ERROR: "login-error",
+
   GET_MATCH: "get-match-by-tablenumber",
   SEND_MATCH: "send-match-to-client",
   SEND_SET: "send-set-to-server",
 
-  // server -> client messages
   AVAILABLE_TABLES: "available-tables",
   START_ROUND: "start-round",
-  LOGIN_ERROR: "login-error",
   NEXT_ROUND: "next-round"
 };
