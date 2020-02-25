1e6e5e459e0e558f7a653ddd9e595bb704774a19
Felix Breitenbach <felix.breitenbach@gmail.com>
2020-02-25 22:00:44 +0100

    update server

--- src/modules/server/server.js
+++ src/modules/server/server.js
@@ -9,10 +9,11 @@ const EventEmitter = require("events");
 // server application
 const expressApp = require("../server/app");
 const socketIOMessages = require("../../client/src/shared/socket-io-messages");
+const { COMPETITION_STATE } = require("../models/competition");
 
 // constants
-const MAX_AMOUNT_TABLE = 16;
-const ALL_POTENTIAL_TABLES = range(1, MAX_AMOUNT_TABLE);
+const MAX_AMOUNT_TABLE = 8;
+const ALL_POTENTIAL_TABLES = range(1, MAX_AMOUNT_TABLE + 1);
 const connectedClients = new Map();
 
 const SocketIOInputEmitter = new EventEmitter();
@@ -58,78 +59,123 @@ function initSocketIO() {
     listenToClientEvent(clientSocket);
   });
 }
-
 function listenToClientEvent(clientSocket) {
   // event fired every time a client sends a table number
-  clientSocket.on(socketIOMessages.LOGIN_TABLE, data => {
-    clientLogin(clientSocket, data);
+  clientSocket.on(socketIOMessages.LOGIN_REQUEST, ({ tableNumber }) => {
+    clientLogin(clientSocket, tableNumber);
   });
 
+  // TODO: remove
   // event fired when a start round is triggered
-  clientSocket.on(socketIOMessages.GET_MATCH, data => {
-    SocketIOOutputEmitter.once(socketIOMessages.SEND_MATCH, data => {
-      clientSocket.emit(socketIOMessages.SEND_MATCH, data);
-    });
-    SocketIOInputEmitter.emit(socketIOMessages.GET_MATCH, data);
-  });
+  // clientSocket.on(socketIOMessages.UPDATE_SETS, data => {
+  //   SocketIOOutputEmitter.once(socketIOMessages.UPDATE_SETS, data => {
+  //     clientSocket.emit(socketIOMessages.UPDATE_SETS, data);
+  //   });
+  //   SocketIOInputEmitter.emit(socketIOMessages.UPDATE_SETS, data);
+  // });
 
   // event fired when a client disconnects, remove it from the list
   clientSocket.on(socketIOMessages.DISCONNECT, () => {
     clientLogout(clientSocket);
   });
+
+  clientSocket.on(socketIOMessages.UPDATE_SETS_REQUEST, data =>
+    updateSets(clientSocket, data)
+  );
 }
 
-function sendStartRoundBroadcast() {
-  if (matchStarted) {
+// CLIENT -> SERVER COMMUNICATION
+
+function updateSets(clientSocket, data) {
+  const { sets, tableNumber, finished } = data;
+  // todo
+  if ("error") {
+    clientSocket.emit(socketIOMessages.UPDATE_SETS_RESPONSE, {
+      message: "something went wrong."
+    });
     return;
   }
+  // update match in memory
+  // emit message to app: new ranking / new match data
+  // save to DB
 
-  matchStarted = true;
-  sendBroadcast(socketIOMessages.START_ROUND, null);
+  clientSocket.emit(socketIOMessages.UPDATE_SETS_RESPONSE, {
+    message: "🎉 ???"
+  });
+  return;
 }
 
 function sendAvailableTablesToClient() {
   sendBroadcast(socketIOMessages.AVAILABLE_TABLES, getAvailableTables());
 }
 
-// this method is used to submit a broadcast event to all clients
-function sendBroadcast(eventName, data) {
-  if (!serverSocket) {
-    return;
-  }
+function getAvailableTables() {
+  const takenTables = Array.from(connectedClients.values()).map(x =>
+    parseInt(x, 10)
+  );
 
-  serverSocket.sockets.emit(eventName, data);
-  console.log(`server emit broadcast: ${eventName}`);
-  console.log(`--- data was ${data}`);
+  const availableTables = ALL_POTENTIAL_TABLES.filter(
+    key => !takenTables.includes(key)
+  );
+
+  return availableTables;
 }
 
-function clientLogin(clientSocket, data) {
-  const { tableNumber } = data;
-
+function clientLogin(clientSocket, tableNumber) {
   // verify if max amount of connected devices/table is reached
   if (connectedClients.size === MAX_AMOUNT_TABLE) {
-    clientSocket.emit(socketIOMessages.LOGIN_ERROR, data);
+    clientSocket.emit(socketIOMessages.LOGIN_RESPONSE, {
+      message: "No free tables available."
+    });
     return;
   }
 
   // verify if a client is already connected to a table
   if (mapHasValue(connectedClients, tableNumber)) {
-    clientSocket.emit(socketIOMessages.LOGIN_ERROR, data);
+    clientSocket.emit(socketIOMessages.LOGIN_RESPONSE, {
+      message: `Table ${tableNumber} is already taken.`
+    });
     return;
   }
 
   // save client socket
   connectedClients.set(clientSocket.id, tableNumber);
-  console.info(`Client login [id=${clientSocket.id}] [table=${tableNumber}]`);
-
-  // send login response to client with his table number
-  clientSocket.emit(socketIOMessages.LOGIN_TABLE, {
-    tableNumber: tableNumber,
-    matchStarted: matchStarted
-  });
 
   // send available tables to clients
   sendAvailableTablesToClient();
+
+  console.info(`Client login [id=${clientSocket.id}] [table=${tableNumber}]`);
+
+  // send login response to client with his table number
+  clientSocket.emit(
+    socketIOMessages.LOGIN_RESPONSE,
+    createLoginResponseData(tableNumber)
+  );
+}
+
+function mapHasValue(inputMap, searchedValue) {
+  const values = Array.from(inputMap.entries());
+  return values.some(([_, value]) => value === searchedValue);
+}
+
+function createLoginResponseData(tableNumber) {
+  const state = "TODO";
+
+  if (
+    state === COMPETITION_STATE.COMP_ACTIVE_ROUND_READY ||
+    state === COMPETITION_STATE.COMP_ACTIVE_ROUND_ACTIVE
+  ) {
+    return {
+      roundAvailable: false,
+      tableNumber,
+      match: {}
+    };
+  }
+
+  return {
+    roundAvailable: false,
+    tableNumber
+  };
 }
 
 function clientLogout(clientSocket) {
@@ -145,23 +191,44 @@ function clientLogout(clientSocket) {
   console.log(`Client gone [id=${clientSocket.id}]`);
 }
 
-function getAvailableTables() {
-  const takenTables = Array.from(connectedClients.values()).map(x =>
-    parseInt(x, 10)
-  );
+// SERVER -> CLIENT COMMUNICATION
 
-  const availableTables = ALL_POTENTIAL_TABLES.filter(
-    key => !takenTables.includes(key)
-  );
+function sendNextRoundBroadcast() {
+  // send match to each client
+  const data = { matches: [] };
 
-  return availableTables;
+  sendBroadcast(socketIOMessages.NEXT_ROUND, data);
 }
 
-function mapHasValue(inputMap, searchedValue) {
-  const values = Array.from(inputMap.entries());
-  return values.some(([_, value]) => value === searchedValue);
+function sendCompetitionCanceledBroadcast() {
+  sendBroadcast(socketIOMessages.COMPETITION_CANCELED);
 }
 
+function sendCancelRoundBroadcast() {
+  sendBroadcast(socketIOMessages.CANCEL_ROUND);
+}
+
+function sendStartRoundBroadcast() {
+  if (matchStarted) {
+    return;
+  }
+
+  matchStarted = true;
+  sendBroadcast(socketIOMessages.START_ROUND);
+}
+
+// this method is used to submit a broadcast event to all clients
+function sendBroadcast(eventName, data) {
+  if (!serverSocket) {
+    return;
+  }
+
+  serverSocket.sockets.emit(eventName, data);
+  console.log(`server emit broadcast: ${eventName}`);
+  console.log(`--- data was ${data}`);
+}
+
+// helper functions
 function range(start, exclusiveEnd) {
   return [...Array(exclusiveEnd).keys()].slice(start);
 }
