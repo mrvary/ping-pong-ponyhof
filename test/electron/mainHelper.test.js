e3bb5673fcbcbbe4a7074010a99336eff30076ca
Felix Breitenbach <felix.breitenbach@gmail.com>
2020-02-26 23:56:55 +0100

    remove comment and add test todos

--- test/electron/mainHelper.test.js
+++ test/electron/mainHelper.test.js
@@ -24,41 +24,8 @@ describe("createStateResponseData()", () => {
     expect(tableNumber).toBe(4);
     expect(roundStarted).toBeFalsy();
   });
+
+  test.todo("COMP_READY_ROUND_READY");
+  test.todo("COMP_ACTIVE_ROUND_READY");
+  test.todo("COMP_ACTIVE_ROUND_ACTIVE");
 });
-// function createStateResponseData({
-//   tableNumber,
-//   competitions,
-//   matchesWithPlayers
-// }) {
-//   const currentlyRunningCompetition = competitions.find(
-//     ({ status }) =>
-//       status === COMPETITION_STATE.COMP_ACTIVE_ROUND_READY ||
-//       status === COMPETITION_STATE.COMP_ACTIVE_ROUND_ACTIVE
-//   );
-
-//   if (!currentlyRunningCompetition) {
-//     return {
-//       roundStarted: false,
-//       tableNumber
-//     };
-//   }
-
-//   const { state } = currentlyRunningCompetition;
-//   const { player1, player2, match } = matchesWithPlayers[tableNumber];
-
-//   if (state === COMPETITION_STATE.COMP_ACTIVE_ROUND_READY) {
-//     return {
-//       roundStarted: false,
-//       tableNumber,
-//       match: { ...match, player1, player2 }
-//     };
-//   }
-
-//   if (state === COMPETITION_STATE.COMP_ACTIVE_ROUND_ACTIVE) {
-//     return {
-//       roundStarted: true,
-//       tableNumber,
-//       match: { ...match, player1, player2 }
-//     };
-//   }
-// }
