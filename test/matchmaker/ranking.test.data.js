6918bba19bdaadb4cd7dd30da7658a7b44bf48e0
Daniel <daniel.n96@t-online.de>
2020-02-26 16:30:37 +0100

    test createMatchResult()

---
+++ test/matchmaker/ranking.test.data.js
@@ -0,0 +1,86 @@
+const matchWithResult_13 = {
+  id: 1,
+  player1: "PLAYER1",
+  player2: "PLAYER2",
+  sets: [
+    {
+      player1: 8,
+      player2: 11
+    },
+    {
+      player1: 11,
+      player2: 13
+    },
+    {
+      player1: 11,
+      player2: 0
+    },
+    {
+      player1: 4,
+      player2: 11
+    }
+  ]
+};
+
+const matchWithResult_12 = {
+  id: 1,
+  player1: "PLAYER1",
+  player2: "PLAYER2",
+  sets: [
+    {
+      player1: 11,
+      player2: 13
+    },
+    {
+      player1: 11,
+      player2: 5
+    },
+    {
+      player1: 4,
+      player2: 11
+    }
+  ]
+};
+
+const matchWithResult_10 = {
+  id: 1,
+  player1: "PLAYER1",
+  player2: "PLAYER2",
+  sets: [
+    {
+      player1: 11,
+      player2: 2
+    }
+  ]
+};
+
+const matchWithWrongSets = {
+  id: 1,
+  player1: "PLAYER1",
+  player2: "PLAYER2",
+  sets: [
+    {
+      player1: 11,
+      player2: 10
+    },
+    {
+      player1: 2,
+      player2: 2
+    },
+    {
+      player1: 0,
+      player2: 0
+    },
+    {
+      player1: 5,
+      player2: 6
+    }
+  ]
+};
+
+module.exports = {
+  matchWithResult_13,
+  matchWithResult_12,
+  matchWithResult_10,
+  matchWithWrongSets
+};
--- test/matchmaker/ranking.test.js
+++ test/matchmaker/ranking.test.js
@@ -8,12 +8,17 @@ const {
   getParameterByPlayerId
 } = require("../../src/matchmaker/ranking");
 
-const {
+const { playersAfterUpdateWinner } = require("./player.test.data");
 
-  playersAfterUpdateWinner
-} = require("./player.test.data");
+const {
+  matchWithResult_13,
+  matchWithResult_12,
+  matchWithResult_10,
+  matchWithWrongSets
+} = require("./ranking.test.data");
 
 describe("createCurrentRanking()", () => {});
+
 describe("calculateBHZ()", () => {
   test("correct BHZ for each player", () => {
     let resultPerPlayer = [];
@@ -23,8 +28,28 @@ describe("calculateBHZ()", () => {
     expect(resultPerPlayer).toEqual([3, 3, 1, 1]);
   });
 });
+
 describe("calculateNewTTR()", () => {});
 describe("getMatchesInvolved()", () => {});
 describe("addMatchDetails()", () => {});
 describe("getParameterByPlayerId()", () => {});
-describe("createMatchResult()", () => {});
+describe("createMatchResult()", () => {
+  test("correct match result was created", () => {
+    const matchResult_13 = createMatchResult(matchWithResult_13);
+    const matchResult_12 = createMatchResult(matchWithResult_12);
+    const matchResult_10 = createMatchResult(matchWithResult_10);
+    const matchResult_00 = createMatchResult(matchWithWrongSets);
+
+    expect(matchResult_13.player1).toBe(1);
+    expect(matchResult_13.player2).toBe(3);
+
+    expect(matchResult_12.player1).toBe(1);
+    expect(matchResult_12.player2).toBe(2);
+
+    expect(matchResult_10.player1).toBe(1);
+    expect(matchResult_10.player2).toBe(0);
+
+    expect(matchResult_00.player1).toBe(0);
+    expect(matchResult_00.player2).toBe(0);
+  });
+});
