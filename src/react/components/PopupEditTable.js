6eafaa8839fb42fb095137290ed6ad0db6cbffca
LAP-BOOK-NOTE-T\willi <superhamster95@gmx.de>
2020-02-26 15:47:43 +0100

    edit Table now with check for correctness

--- src/react/components/PopupEditTable.js
+++ src/react/components/PopupEditTable.js
@@ -6,9 +6,7 @@ import Button from './Button';
 
 function PopupEditTable({ show, handleClose, sets, saveChanges }) {
   const [inputChanged, setInputChanged] = useState(false);
-  const handleInputChanged = () => {
-    setInputChanged(true);
-  };
+
   const endPopup = () => {
     setInputChanged(false);
     saveChanges();
@@ -20,7 +18,7 @@ function PopupEditTable({ show, handleClose, sets, saveChanges }) {
       <Modal.Body>
         <DisplaySetHandler
           sets={sets}
-          handleInputChanged={handleInputChanged}
+          setInputChanged={setInputChanged}
         ></DisplaySetHandler>
         <Button
           mode="primary"
@@ -32,7 +30,7 @@ function PopupEditTable({ show, handleClose, sets, saveChanges }) {
     </Modal>
   );
 }
-const DisplaySetHandler = ({ sets, handleInputChanged }) => {
+const DisplaySetHandler = ({ sets, setInputChanged }) => {
   let index = 0;
   return (
     <div className="popupEditTable--columns">
@@ -42,33 +40,74 @@ const DisplaySetHandler = ({ sets, handleInputChanged }) => {
           <DisplaySet
             set={set}
             index={index}
-            handleInputChanged={handleInputChanged}
+            setInputChanged={setInputChanged}
           ></DisplaySet>
         );
       })}
     </div>
   );
 };
-const DisplaySet = ({ set, index, handleInputChanged }) => {
+const DisplaySet = ({ set, index, setInputChanged }) => {
+  let [player1Set, setPlayer1Set] = useState(set.player1);
+  let [player2Set, setPlayer2Set] = useState(set.player2);
+  let [css, setCss] = useState('');
+
+  const checkInput1 = event => {
+    setPlayer1Set(event.target.value);
+
+    console.log(player1Set + ' ' + player2Set);
+    if (
+      (player1Set === 11 && player2Set < 10) ||
+      (player2Set === 11 && player1Set < 10) ||
+      (player1Set >= 10 && player1Set - player2Set === 2) ||
+      (player2Set >= 10 && player2Set - player1Set === 2)
+    ) {
+      setInputChanged(true);
+    } else {
+      setInputChanged(false);
+      setCss('popupEditTable--input');
+    }
+  };
+
+  const checkInput2 = event => {
+    setPlayer2Set(event.target.value);
+
+    console.log(player1Set + ' ' + player2Set);
+    if (player1Set - player2Set >= 2 && (player1Set > 10 || player2Set > 10)) {
+      setInputChanged(true);
+      setCss('');
+    } else {
+      setInputChanged(false);
+      setCss('popupEditTable--input');
+    }
+  };
+  //ganze zahlen
   return (
     <div className="popupEditTable--rows">
       <p>{'Set ' + index + '  '}</p>
       <input
+        className={css}
         size="2"
         maxLength="2"
-        defaultValue={set.player1}
-        def
-        onChange={handleInputChanged}
+        value={player1Set}
+        onChange={checkInput1}
       ></input>
       <p> {' : '} </p>
       <input
+        className={css}
         size="2"
         maxLength="2"
-        defaultValue={set.player2}
-        onChange={handleInputChanged}
+        value={player2Set}
+        onChange={checkInput2}
       ></input>
     </div>
   );
 };
 
 export default PopupEditTable;
+/*
+        value={player1Set}
+
+        value={player2Set}
+
+*/
