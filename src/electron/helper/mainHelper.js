69f78b8e5f8a1121106981eec4571e0d2101517c
Felix Breitenbach <felix.breitenbach@gmail.com>
2020-02-27 01:11:15 +0100

    fix typo

--- src/electron/helper/mainHelper.js
+++ src/electron/helper/mainHelper.js
@@ -18,10 +18,10 @@ function createStateResponseData({
     };
   }
 
-  const { state } = currentlyRunningCompetition;
+  const { status } = currentlyRunningCompetition;
   const { player1, player2, match } = matchesWithPlayers[tableNumber];
 
-  if (state === COMPETITION_STATE.COMP_ACTIVE_ROUND_READY) {
+  if (status === COMPETITION_STATE.COMP_ACTIVE_ROUND_READY) {
     return {
       roundStarted: false,
       tableNumber,
@@ -29,7 +29,7 @@ function createStateResponseData({
     };
   }
 
-  if (state === COMPETITION_STATE.COMP_ACTIVE_ROUND_ACTIVE) {
+  if (status === COMPETITION_STATE.COMP_ACTIVE_ROUND_ACTIVE) {
     return {
       roundStarted: true,
       tableNumber,
