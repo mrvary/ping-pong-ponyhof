25bf3d9ed8cd313e2a3bb023ed7e5ab81dcf5160
LAP-BOOK-NOTE-T\willi <superhamster95@gmx.de>
2020-02-27 22:31:35 +0100

    first attempt

--- src/react/components/CompetitionPage.js
+++ src/react/components/CompetitionPage.js
@@ -1,29 +1,30 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import "./CompetitionPage.css";
-import "../Colors.css";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import './CompetitionPage.css';
+import '../Colors.css';
 
 //componenten
-import Popup from "./Popup";
-import Footer from "./Footer";
-import Button from "./Button";
-import CompetitionPageHeader from "./CompetitionPageHeader";
-import PopupEditTable from "./PopupEditTable";
+import Popup from './Popup';
+import Footer from './Footer';
+import Button from './Button';
+import CompetitionPageHeader from './CompetitionPageHeader';
+import PopupEditTable from './PopupEditTable';
 
 // ipc communication
-import IPCService from "../../shared/ipc/ipcRendererService";
+import IPCService from '../../shared/ipc/ipcRendererService';
+import CompetitionPage__Header from './CompetitionPageHeader';
 const ipcRenderer = window.electron.ipcRenderer;
-const ipcChannels = require("../../shared/ipc-messages");
+const ipcChannels = require('../../shared/ipc-messages');
 
 const USE_BROWSER = false;
 
 const IpAdressAndStatisticLink = ({ competitionID, openStatisticWindow }) => {
-  const statisticID = "/statisticTable/" + competitionID;
+  const statisticID = '/statisticTable/' + competitionID;
   return (
     <div className="competitionPage__link-alignment">
       <div className="competitionPage__link-ip-adress-statistic">
-        {" "}
-        IP-Adresse{" "}
+        {' '}
+        IP-Adresse{' '}
       </div>
       <p
         onClick={() => openStatisticWindow(statisticID)}
@@ -40,44 +41,44 @@ const TableHeadline = () => {
     <div className="competitionPage__centered">
       <div className="competitionPage__table competitionPage__table--def">
         <strong className="competitionPage__table--elements competitionPage__centered">
-          {" "}
-          Tisch{" "}
+          {' '}
+          Tisch{' '}
         </strong>
         <strong className="competitionPage__table--elements competitionPage__centered">
-          {" "}
+          {' '}
           Spieler 1
         </strong>
         <strong className="competitionPage__table--elements competitionPage__centered">
-          {" "}
-          :{" "}
+          {' '}
+          :{' '}
         </strong>
         <strong className="competitionPage__table--elements competitionPage__centered">
-          {" "}
-          Spieler 2{" "}
+          {' '}
+          Spieler 2{' '}
         </strong>
         <strong className="competitionPage__table--elements competitionPage__centered">
-          {" "}
-          Satz 1{" "}
+          {' '}
+          Satz 1{' '}
         </strong>
         <strong className="competitionPage__table--elements competitionPage__centered">
-          {" "}
-          Satz 2{" "}
+          {' '}
+          Satz 2{' '}
         </strong>
         <strong className="competitionPage__table--elements competitionPage__centered">
-          {" "}
-          Satz 3{" "}
+          {' '}
+          Satz 3{' '}
         </strong>
         <strong className="competitionPage__table--elements competitionPage__centered">
-          {" "}
-          Satz 4{" "}
+          {' '}
+          Satz 4{' '}
         </strong>
         <strong className="competitionPage__table--elements competitionPage__centered">
-          {" "}
-          Satz 5{" "}
+          {' '}
+          Satz 5{' '}
         </strong>
         <strong className="competitionPage__table--elements competitionPage__centered">
-          {" "}
-          Ergebnis{" "}
+          {' '}
+          Ergebnis{' '}
         </strong>
       </div>
     </div>
@@ -86,16 +87,16 @@ const TableHeadline = () => {
 
 const TableRow = ({ match, activ }) => {
   const [stringSet, setStringSet] = useState([
-    "0 : 0",
-    "0 : 0",
-    "0 : 0",
-    "0 : 0",
-    "0 : 0"
+    '0 : 0',
+    '0 : 0',
+    '0 : 0',
+    '0 : 0',
+    '0 : 0'
   ]);
   let index = 0;
 
   match.sets.forEach(set => {
-    stringSet[index] = set.player1 + " : " + set.player2;
+    stringSet[index] = set.player1 + ' : ' + set.player2;
     index++;
   });
 
@@ -107,46 +108,55 @@ const TableRow = ({ match, activ }) => {
     //TODO save Changes from edited Table
     handleCloseEditMatch();
   };
-
+  let tischCss = 'competition__table--tisch';
+  if (false) {
+    tischCss = 'competition__table--tisch--online';
+  }
+  /* <span className={tischCss}>&#8226;</span>
+          <span>&#xa0;</span>
+          <span>&#xa0;</span>
+          */
   return (
     <div className="competitionPage__centered">
       <div className="competitionPage__table competitionPage__table--values">
-        <div className="competitionPage__table--elements competitionPage__centered"></div>
         <div className="competitionPage__table--elements competitionPage__centered">
-          {" "}
+          <li className={tischCss}>1</li>
+        </div>
+        <div className="competitionPage__table--elements competitionPage__centered">
+          {' '}
           {match.player1}
         </div>
         <div className="competitionPage__table--elements competitionPage__centered">
-          {" "}
-          :{" "}
+          {' '}
+          :{' '}
         </div>
         <div className="competitionPage__table--elements competitionPage__centered">
-          {" "}
-          {match.player2}{" "}
+          {' '}
+          {match.player2}{' '}
         </div>
         <div className="competitionPage__table--elements competitionPage__centered">
-          {" "}
-          {stringSet[0]}{" "}
+          {' '}
+          {stringSet[0]}{' '}
         </div>
         <div className="competitionPage__table--elements competitionPage__centered">
-          {" "}
-          {stringSet[1]}{" "}
+          {' '}
+          {stringSet[1]}{' '}
         </div>
         <div className="competitionPage__table--elements competitionPage__centered">
-          {" "}
-          {stringSet[2]}{" "}
+          {' '}
+          {stringSet[2]}{' '}
         </div>
         <div className="competitionPage__table--elements competitionPage__centered">
-          {" "}
-          {stringSet[3]}{" "}
+          {' '}
+          {stringSet[3]}{' '}
         </div>
         <div className="competitionPage__table--elements competitionPage__centered">
-          {" "}
-          {stringSet[4]}{" "}
+          {' '}
+          {stringSet[4]}{' '}
         </div>
         <div className="competitionPage__table--elements competitionPage__centered">
-          {" "}
-          Ergebnis{" "}
+          {' '}
+          Ergebnis{' '}
         </div>
         <button
           onClick={handleShowEditMatch}
@@ -168,7 +178,7 @@ const TableRow = ({ match, activ }) => {
 
 const Table = ({ matches, activ }) => {
   let tableCss =
-    "competitionPage__table" + (activ ? "--barrierGreen" : "--barrierRed");
+    'competitionPage__table' + (activ ? '--barrierGreen' : '--barrierRed');
   return (
     <div className={tableCss}>
       <TableHeadline />
@@ -185,7 +195,7 @@ const CompetitionPage = () => {
 
   useEffect(() => {
     function handleMatchesStatusChanged(event, { matchesWithPlayers }) {
-      console.log("IPC-Main-->IPC-Renderer:", matchesWithPlayers);
+      console.log('IPC-Main-->IPC-Renderer:', matchesWithPlayers);
       const matches = mapPlayerNamesToMatch(matchesWithPlayers);
       setMatches(matches);
     }
@@ -206,8 +216,8 @@ const CompetitionPage = () => {
       const matches = [
         {
           id: 3,
-          player1: "Samuel Geiger",
-          player2: "Marius Bach",
+          player1: 'Samuel Geiger',
+          player2: 'Marius Bach',
           sets: [
             { player1: 11, player2: 13 },
             { player1: 4, player2: 11 }
@@ -217,8 +227,8 @@ const CompetitionPage = () => {
         },
         {
           id: 4,
-          player1: "Edith Finch",
-          player2: "Finch Assozial",
+          player1: 'Edith Finch',
+          player2: 'Finch Assozial',
           sets: [
             { player1: 13, player2: 15 },
             { player1: 14, player2: 16 }
@@ -250,8 +260,8 @@ const CompetitionPage = () => {
   function mapPlayerNamesToMatch(matchesWithPlayers) {
     return matchesWithPlayers.map(matchWithPlayers => {
       const { match, player1, player2 } = matchWithPlayers;
-      match.player1 = player1.firstname + " " + player1.lastname;
-      match.player2 = player2.firstname + " " + player2.lastname;
+      match.player1 = player1.firstname + ' ' + player1.lastname;
+      match.player2 = player2.firstname + ' ' + player2.lastname;
       return match;
     });
   }
@@ -292,7 +302,7 @@ const CompetitionPage = () => {
         playmode="Scheizer System"
         startDate="02.02.2020"
         linkTitle="zur Übersicht"
-        linkDestination={"/"}
+        linkDestination={'/'}
       />
       <IpAdressAndStatisticLink
         competitionID={competitionID}
@@ -339,7 +349,7 @@ const CompetitionPage = () => {
           primText="Spiel starten"
           secOnClick={handleShowGoInactive}
           secText="Spiel pausieren"
-          mode={active ? "secondary" : "primary"}
+          mode={active ? 'secondary' : 'primary'}
         ></Button>
         <Popup
           show={showPopupGoInactive}
