c7327065784692d03ae3a4787292037dabde8ea1
LAP-BOOK-NOTE-T\willi <superhamster95@gmx.de>
2020-03-03 20:47:42 +0100

    better button

--- src/react/components/PopupReviewPlayer.js
+++ src/react/components/PopupReviewPlayer.js
@@ -1,8 +1,8 @@
-import { Modal } from "react-bootstrap";
-import React, { useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./PopupReviewPlayer.css";
-import Button from "./Button";
+import { Modal } from 'react-bootstrap';
+import React, { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './PopupReviewPlayer.css';
+import Button from './Button';
 
 function PopupReviewPlayer({
   show,
@@ -19,16 +19,20 @@ function PopupReviewPlayer({
 
       <Modal.Body>
         <div className="popup__centered">
-          <p className="popup__body-small-text">
-            Datum: {viewedCompetition.date}
+          <strong className="popup__body-small-text">
+            {viewedCompetition.name}
+          </strong>
+          <p className="popup__body-smaller-text">{viewedCompetition.date}</p>
+          <p className="popup__body-smaller-text popup__extra-margin">
+            {viewedCompetition.playmode}
           </p>
           <p className="popup__body-small-text">
-            Folgende Spieler sind im Spiel:
+            Folgende Spieler sind im Turnier:
           </p>
-          <div>
+          <div className="popup__player-List">
             {viewedPlayers.map(player => (
               <li key={player.id} className="popup__names">
-                {player.firstname + " " + player.lastname}
+                {player.firstname + ' ' + player.lastname}
               </li>
             ))}
           </div>
