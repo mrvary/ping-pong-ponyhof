c63c9948a0d7e1f96ac737c16380288ae2dc243e
Felix Breitenbach <felix.breitenbach@gmail.com>
2020-03-04 01:49:00 +0100

    switch to select based input

--- src/client/src/react/components/ScoreBoard.js
+++ src/client/src/react/components/ScoreBoard.js
@@ -5,9 +5,20 @@ import MainScore from "./MainScore";
 import Set from "./Set";
 import { isMatchFinished } from "../../shared/lib";
 
+const MAX_INPUT = 50;
+
 function ScoreBoard({ match, sendSets, updateSets, addSet }) {
   // const showSets = () =>
   //   match.sets.map((set, index) => <Set key={index} index={index} set={set} />);
+  const generateOptions = () => {
+    const numberArray = [...Array(MAX_INPUT).keys()];
+    const options = numberArray.map(number => (
+      <option key={number} value={number}>
+        {number}
+      </option>
+    ));
+    return options;
+  };
 
   const { sets } = match;
   return (
@@ -17,20 +28,34 @@ function ScoreBoard({ match, sendSets, updateSets, addSet }) {
         {sets.map((set, index) => {
           return (
             <div key={index} className="score-board__set-container">
-              <input
+              <datalist id="score-numbers">
+                <option value="0" />
+                <option value="1" />
+                <option value="2" />
+                <option value="3" />
+                <option value="4" />
+              </datalist>
+              <select
+                list="score-numbers"
                 className="score-board__set-container-item"
                 type="number"
                 min="0"
                 value={set.player1}
                 onChange={updateSets(match)("player1")(index)}
-              ></input>
-              <input
+              >
+                {generateOptions()}
+              </select>
+              <span className="score-board__set-delimiter">:</span>
+              <select
+                list="score-numbers"
                 className="score-board__set-container-item"
                 type="number"
                 min="0"
                 value={set.player2}
                 onChange={updateSets(match)("player2")(index)}
-              ></input>
+              >
+                {generateOptions()}
+              </select>
             </div>
           );
         })}
