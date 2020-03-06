a99a28718304341826239b5a6b490299b0c54124
LAP-BOOK-NOTE-T\willi <superhamster95@gmx.de>
2020-03-06 10:32:24 +0100

    changed button hover function

--- src/react/App.js
+++ src/react/App.js
@@ -1,33 +1,33 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import "./Colors.css";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import './Colors.css';
 
 // dummy data
-import dummyCompetitions from "../assets/mock-data/competitions.mock.data";
+import dummyCompetitions from '../assets/mock-data/competitions.mock.data';
 
 // components
-import Footer from "./components/Footer";
-import Competition from "./components/Competition";
-import Header from "./components/Header";
+import Footer from './components/Footer';
+import Competition from './components/Competition';
+import Header from './components/Header';
 
 // electron
 const ipcRenderer = window.electron.ipcRenderer;
-const ipcMessages = require("../shared/ipc-messages");
-const COMPETITION_STATE = require("../shared/models/competition-state");
+const ipcMessages = require('../shared/ipc-messages');
+const COMPETITION_STATE = require('../shared/models/competition-state');
 // set to true for fake backend data and skip IPC calls
 const USE_BROWSER = false;
 
 let competitionID = null;
 
 const App = () => {
-  const [currentId, setCurrentId] = useState("");
+  const [currentId, setCurrentId] = useState('');
   const [linkDisabled, setLinkDisabled] = useState(true);
   const [competitions, setCompetitions] = useState([]);
 
   const [viewedCompetition, setViewedCompetition] = useState({});
   const [viewedPlayers, setViewedPlayers] = useState([]);
 
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState('');
   const [hasActiveGame, setHasActiveGame] = useState(false);
 
   useEffect(() => {
@@ -51,38 +51,38 @@ const App = () => {
     // trigger event to get competitions from ipc-main
     ipcRenderer.send(ipcMessages.GET_COMPETITIONS_REQUEST);
     console.log(
-      "ipc-renderer --> ipc-main:",
+      'ipc-renderer --> ipc-main:',
       ipcMessages.GET_COMPETITIONS_REQUEST
     );
   };
 
   const updateActiveState = competitions => {
     setHasActiveGame(false);
-    console.log("started here");
+    console.log('started here');
     competitions.map(competition => {
       if (
         competition.state === COMPETITION_STATE.COMP_ACTIVE_ROUND_ACTIVE ||
         competition.state === COMPETITION_STATE.COMP_ACTIVE_ROUND_READY
       ) {
         setHasActiveGame(true);
-        console.log("and past here");
+        console.log('and past here');
       }
       return null;
     });
-    console.log("hasActiveGame", hasActiveGame);
+    console.log('hasActiveGame', hasActiveGame);
   };
 
   const openXMLDialog = () => {
     ipcRenderer.once(ipcMessages.OPEN_FILE_DIALOG_RESPONSE, (event, args) => {
       console.log(
-        "ipc-main --> ipc-renderer:",
+        'ipc-main --> ipc-renderer:',
         ipcMessages.OPEN_FILE_DIALOG_RESPONSE
       );
 
       const { message } = args;
-      console.log("message:", message);
+      console.log('message:', message);
 
-      if (message === "success") {
+      if (message === 'success') {
         getCompetition();
 
         setLinkDisabled(false);
@@ -97,7 +97,7 @@ const App = () => {
       ipcMessages.GET_COMPETITION_PREVIEW_RESPONSE,
       (event, args) => {
         console.log(
-          "ipc-main --> ipc-renderer",
+          'ipc-main --> ipc-renderer',
           ipcMessages.GET_COMPETITION_PREVIEW_RESPONSE
         );
         const { competition, players } = args;
@@ -114,7 +114,7 @@ const App = () => {
   const importXML = handleShowError => {
     ipcRenderer.once(ipcMessages.IMPORT_XML_FILE_RESPONSE, (event, args) => {
       console.log(
-        "ipc-main --> ipc-renderer:",
+        'ipc-main --> ipc-renderer:',
         ipcMessages.IMPORT_XML_FILE_RESPONSE
       );
       const { competitionId, message } = args;
