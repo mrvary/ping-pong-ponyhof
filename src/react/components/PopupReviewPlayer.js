31cc87fc1b29e827ab0ed98f0278221eace3ddd8
LAP-BOOK-NOTE-T\willi <superhamster95@gmx.de>
2020-03-10 15:46:50 +0100

    fixed freilos in popupReviewPlayer

--- src/react/components/PopupReviewPlayer.js
+++ src/react/components/PopupReviewPlayer.js
@@ -1,8 +1,8 @@
-import { Modal } from "react-bootstrap";
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./PopupReviewPlayer.css";
-import Button from "./Button";
+import { Modal } from 'react-bootstrap';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './PopupReviewPlayer.css';
+import Button from './Button';
 
 function PopupReviewPlayer({
   show,
@@ -11,6 +11,8 @@ function PopupReviewPlayer({
   viewedPlayers,
   viewedCompetition
 }) {
+  console.log('players:', viewedPlayers);
+  const playerList = viewedPlayers.filter(player => player.id !== 'FreeTicket');
   return (
     <Modal show={show} onHide={() => handleClose(true)}>
       <Modal.Header closeButton className="popupReviewPlayer__header-text">
@@ -29,12 +31,12 @@ function PopupReviewPlayer({
             {viewedCompetition.playmode}
           </p>
           <p className="popupReviewPlayer__body-small-text">
-            Folgende {viewedPlayers.length} Spieler sind im Turnier:
+            Folgende {playerList.length} Spieler sind im Turnier:
           </p>
           <div className="popupReviewPlayer__player-List">
-            {viewedPlayers.map(player => (
+            {playerList.map(player => (
               <li key={player.id} className="popupReviewPlayer__names">
-                {player.firstname + " " + player.lastname}
+                {player.firstname + ' ' + player.lastname}
               </li>
             ))}
           </div>
