57fb96ad863dfc164568d5079509ee462ef1329c
Felix Breitenbach <felix.breitenbach@gmail.com>
2020-03-12 02:51:43 +0100

    remove unused components from scoreboard

--- src/client/src/react/components/ScoreBoard.js
+++ src/client/src/react/components/ScoreBoard.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ScoreBoard.css";
 
 import MainScore from "./MainScore";
-import Set from "./Set";
 import {
   isMatchFinished,
   setsWonPlayer1,
